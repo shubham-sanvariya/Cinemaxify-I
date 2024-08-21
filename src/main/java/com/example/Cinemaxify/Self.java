933232da3481d4ca4b319e4286202fca250e47a4
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-08-21 17:43:36 +0530

    implementing user

--- src/main/java/com/example/Cinemaxify/Self.java
+++ src/main/java/com/example/Cinemaxify/Self.java
@@ -1,12 +1,27 @@
 package com.example.Cinemaxify;
 
 // Override the User Interface methods in Self class
-public class Self{
+public class Self implements User{
 
     private String name;
     private String memberName = "self";
     private int age;
     private Long contact;
     private String address;
+
+    @Override
+    public void setUserDetails(String name, int age, Long contact, String address) {
+        this.name = name;
+        this.age = age;
+        this.contact = contact;
+        this.address = address;
+    }
+    @Override
+    public void getUserDetails() {
+        System.out.println("Hello " + name + " you have entered the following details for " + memberName);
+        System.out.println("age: " + age);
+        System.out.println("contact: " + contact);
+        System.out.println("address: " + address);
+    }
 
 }
--- src/main/java/com/example/Cinemaxify/Spouse.java
+++ src/main/java/com/example/Cinemaxify/Spouse.java
@@ -1,11 +1,26 @@
 package com.example.Cinemaxify;
 
 // Override the User Interface methods in Spouse class
-public class Spouse{
+public class Spouse implements User{
     private String name;
     private String memberName = "spouse";
     private int age;
     private Long contact;
     private String address;
+
+@Override
+public void setUserDetails(String name, int age, Long contact, String address) {
+    this.name = name;
+    this.age = age;
+    this.contact = contact;
+    this.address = address;
+}
+@Override
+public void getUserDetails() {
+    System.out.println("Hello " + name + " you have entered the following details for " + memberName);
+    System.out.println("age: " + age);
+    System.out.println("contact: " + contact);
+    System.out.println("address: " + address);
+};
 
 }
